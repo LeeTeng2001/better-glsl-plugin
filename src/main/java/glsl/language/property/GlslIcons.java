d6aa24d4a2355808b6fb8dc9e5656e0104063714
Bryan Lee <leeteng2001@gmail.com>
2022-04-25 15:26:33 +0800

    Fix icon deprecation warning

--- src/main/java/glsl/language/property/GlslIcons.java
+++ src/main/java/glsl/language/property/GlslIcons.java
@@ -3,7 +3,6 @@ package glsl.language.property;
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
-
 public class GlslIcons {
-    public static final Icon FILE = IconLoader.getIcon("icons/glsl.png");
+    public static final Icon FILE = IconLoader.getIcon("icons/glsl.png", GlslIcons.class);
 }
